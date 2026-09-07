8599f94b41e23fedaec7e327c81ceebbed7a62de
Gert07 <puuramartin@gmail.com>
2026-09-07 15:15:23 +0300

    Projektide loomine
    Lõin järgnevad projektid: TaRge25Shop.ApplicationServices, TaRge25Shop.Core, TaRge25Shop.Data ja data kausta installeerisin Microsoft.EntityFrameworkCore

---
+++ TaRge25.Data/TaRge25ShopContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaRge25Shop.Data
+{
+    public class TaRge25ShopContext : DbContext
+    {
+        //see class tuleb teha DbContextist, et saaksime kasutada
+        //Entity Frameworki andmebaasi operatsioone
+        public TaRge25ShopContext(DbContextOptions<TaRge25ShopContext> options) : base(options)
+        {
+        }
+    }
+}
