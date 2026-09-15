3f3aec9b9b0deb252c7d644bfd5da2c540440747
Gert07 <puuramartin@gmail.com>
2026-09-15 11:29:08 +0300

    Katse läbi

--- TaRge25Shop/Controllers/HomeController.cs
+++ TaRge25Shop/Controllers/HomeController.cs
@@ -21,6 +21,5 @@ namespace TaRge25Shop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        testing
     }
 }
